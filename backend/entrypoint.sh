62cdaa10aa88b13c41316283c95fb321f8f16cff
0xPierre <0xpierre@protonmail.com>
2023-10-14 00:42:57 +0200

    feat: setup backend

---
+++ backend/entrypoint.sh
@@ -0,0 +1,9 @@
+#!/bin/bash
+
+until PGPASSWORD=$DB_PASSWORD psql -h $DB_HOST -U $DB_USER -d $DB_DB -c 'SELECT 1' &> /dev/null; do
+  echo "PostgreSQL is unavailable - sleeping"
+  sleep 1
+done
+
+echo "PostgreSQL is up - executing command"
+exec "$@"
