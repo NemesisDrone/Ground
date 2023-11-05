1a0fd91d2595ad01770c8c9b794d79f52fc7f2d7
Emilio BRIAND <ebriand@et.esiea.fr>
2023-11-05 15:09:43 +0100

    test emilio

--- frontend/components/Dashboard/VideoStreaming.vue
+++ frontend/components/Dashboard/VideoStreaming.vue
@@ -16,7 +16,7 @@ const closeWindow = () => {
   <div class="relative h-full m-0 p-0 overflow-hidden">
     <img
       class="w-full h-full object-cover"
-      src="https://media.tenor.com/UuLJ3JKB_PQAAAAd/bomb-explosion.gif"
+      src="https://media.tenor.com/_F5jZzS4RnwAAAAC/twerk.gif"
     />
     <button
       class="absolute top-0 right-0 z-50 bg-neutral-900 rounded p-1.5 mt-2.5 mr-2.5 text-primary"
