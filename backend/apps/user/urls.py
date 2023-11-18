1ae91c5b3d37afd69c47eae2322c001d88fadc7b
0xPierre <0xpierre@protonmail.com>
2023-11-18 19:19:07 +0100

    feat: add user data handling

--- backend/apps/user/urls.py
+++ backend/apps/user/urls.py
@@ -1,10 +1,14 @@
 from django.urls import path
 from rest_framework_simplejwt.views import TokenRefreshView, TokenBlacklistView
+from rest_framework import routers
 
 from . import views
 
+
+
 urlpatterns = [
-    path('token', views.UserLoginView.as_view(), name='token_obtain_pair'),
-    path('refresh', TokenRefreshView.as_view(), name='token_refresh'),
-    path("blacklist", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path('api/user/token', views.UserLoginView.as_view(), name='token_obtain_pair'),
+    path('api/user/refresh', TokenRefreshView.as_view(), name='token_refresh'),
+    path("api/user/blacklist", TokenBlacklistView.as_view(), name="token_blacklist"),
+    path("api/user/data", views.get_user_data, name="user_data"),
 ]
--- backend/apps/user/views.py
+++ backend/apps/user/views.py
@@ -1,5 +1,21 @@
-from rest_framework import generics, permissions
+from rest_framework import viewsets, permissions
+from rest_framework.decorators import api_view, permission_classes
+from rest_framework.request import Request
+from rest_framework.response import Response
 from rest_framework_simplejwt.views import TokenObtainPairView
+from .serializers import UserSerializer
+from ..user.models import User
 
 class UserLoginView(TokenObtainPairView):
     permission_classes = (permissions.AllowAny,)
+
+
+@api_view(['GET'])
+@permission_classes([permissions.IsAuthenticated])
+def get_user_data(request: Request):
+    """
+    Get user data
+    :return:
+    """
+    serializer = UserSerializer(request.user)
+    return Response(serializer.data)
