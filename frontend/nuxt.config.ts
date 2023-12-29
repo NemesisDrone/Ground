d2316218e7c474f58dc9ddfa540b06a5fd31040c
0xPierre <0xpierre@protonmail.com>
2023-12-29 14:40:40 +0100

    fix: remove page transition to fix layout

--- frontend/nuxt.config.ts
+++ frontend/nuxt.config.ts
@@ -1,12 +1,8 @@
 // https://nuxt.com/docs/api/configuration/nuxt-config
 export default defineNuxtConfig({
   devtools: { enabled: false },
-  app: {
-    pageTransition: { name: 'page', mode: 'out-in' }
-  },
   runtimeConfig: {
     public: {
-      SOCKETIO_URL: process.env.SOCKETIO_URL,
       API_URL: process.env.API_URL,
       WEB_SOCKET_COMMUNICATION_URL:
         process.env.WEB_SOCKET_COMMUNICATION_URL
