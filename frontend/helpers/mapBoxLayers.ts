4c2d2b9e14d311812497406f66b696b83ade0a83
0xPierre <0xpierre@protonmail.com>
2024-01-01 21:17:45 +0100

    refacto: mapbox 3D layer

---
+++ frontend/helpers/mapBoxLayers.ts
@@ -0,0 +1,115 @@
+import mapboxgl, { CustomLayerInterface } from 'mapbox-gl'
+import { useSensorsStore } from '~/store/sensors'
+import * as THREE from 'three'
+// @ts-ignore
+import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+
+export const getMapBox3DDroneModelLayer = (
+  camera: THREE.Camera | null,
+  scene: THREE.Scene | null,
+  map: mapboxgl.Map
+): mapboxgl.CustomLayerInterface => {
+  const sensorStore = useSensorsStore()
+
+  const layer: mapboxgl.CustomLayerInterface = {
+    id: 'drone-model',
+    type: 'custom',
+    renderingMode: '3d',
+    onAdd: function (map, gl) {
+      camera = new THREE.Camera()
+      scene = new THREE.Scene()
+
+      // create two lights to illuminate the model
+      const directionalLight = new THREE.DirectionalLight(0xffffff)
+      directionalLight.position.set(0, -70, 100).normalize()
+      scene.add(directionalLight)
+
+      const directionalLight2 = new THREE.DirectionalLight(0xffffff)
+      directionalLight2.position.set(0, 70, 100).normalize()
+      scene.add(directionalLight2)
+
+      //add the 3D model to the three scene
+      const loader = new GLTFLoader()
+      //  @ts-ignore
+      loader.load('/mq-9_reaper/scene.gltf', (gltf) => {
+        ;(scene as THREE.Scene).add(gltf.scene)
+      })
+      // @ts-ignore
+      this.map = map
+      // @ts-ignore
+      this.renderer = new THREE.WebGLRenderer({
+        canvas: map.getCanvas(),
+        context: gl,
+        antialias: true
+      })
+
+      // @ts-ignore
+      this.renderer.autoClear = false
+    },
+    render: function (gl, matrix) {
+      const modelOrigin: [number, number] = [
+        sensorStore.gpsPosition.lat,
+        sensorStore.gpsPosition.lng
+      ]
+      const modelAltitude = 10
+      const modelRotate = [Math.PI / 2, 0, 0]
+
+      const modelAsMercatorCoordinate =
+        mapboxgl.MercatorCoordinate.fromLngLat(modelOrigin, modelAltitude)
+      const modelTransform = {
+        translateX: modelAsMercatorCoordinate.x,
+        translateY: modelAsMercatorCoordinate.y,
+        translateZ: modelAsMercatorCoordinate.z as number,
+        rotateX: modelRotate[0],
+        rotateY: modelRotate[1],
+        rotateZ: modelRotate[2],
+        /* Since the 3D model is in real world meters, a scale transform needs to be applied
+         */
+        scale:
+          modelAsMercatorCoordinate.meterInMercatorCoordinateUnits() * 6
+      }
+
+      const rotationX = new THREE.Matrix4().makeRotationAxis(
+        new THREE.Vector3(1, 0, 0),
+        modelTransform.rotateX
+      )
+      const rotationY = new THREE.Matrix4().makeRotationAxis(
+        new THREE.Vector3(0, 1, 0),
+        modelTransform.rotateY
+      )
+      const rotationZ = new THREE.Matrix4().makeRotationAxis(
+        new THREE.Vector3(0, 0, 1),
+        modelTransform.rotateZ
+      )
+
+      const m = new THREE.Matrix4().fromArray(matrix)
+      const l = new THREE.Matrix4()
+        .makeTranslation(
+          modelTransform.translateX,
+          modelTransform.translateY,
+          modelTransform.translateZ
+        )
+        .scale(
+          new THREE.Vector3(
+            modelTransform.scale,
+            -modelTransform.scale,
+            modelTransform.scale
+          )
+        )
+        .multiply(rotationX)
+        .multiply(rotationY)
+        .multiply(rotationZ)
+
+      // @ts-ignore
+      camera.projectionMatrix = m.multiply(l)
+      // @ts-ignore
+      this.renderer.resetState()
+      // @ts-ignore
+      this.renderer.render(scene, camera)
+      // @ts-ignore
+      this.map.triggerRepaint()
+    }
+  }
+
+  return layer
+}
