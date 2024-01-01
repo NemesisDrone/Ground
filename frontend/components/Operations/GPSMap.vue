08fd530cab4aaf37c25eee12a7ac21208ee757eb
0xPierre <0xpierre@protonmail.com>
2024-01-01 21:20:52 +0100

    feat: add map view toggle

--- frontend/components/Operations/GPSMap.vue
+++ frontend/components/Operations/GPSMap.vue
@@ -5,7 +5,8 @@ import {
   LocateFixed,
   LocateOff,
   ScanSearch,
-  Box
+  Box,
+  Map
 } from 'lucide-vue-next'
 import { useSensorsStore } from '~/store/sensors'
 import { storeToRefs } from 'pinia'
@@ -13,6 +14,7 @@ import { v4 as uuidv4 } from 'uuid'
 import mapboxgl from 'mapbox-gl'
 import * as THREE from 'three'
 import { GLTFLoader } from 'three/addons/loaders/GLTFLoader.js'
+import { getMapBox3DDroneModelLayer } from '~/helpers/mapBoxLayers'
 
 const sensorsStore = useSensorsStore()
 const { gpsPosition } = storeToRefs(sensorsStore)
@@ -32,9 +34,6 @@ onMounted(() => {
     loadMap.value = true
     // Set default center to esiea position
   }, 1500)
-  setInterval(() => {
-    pos.value += 0.00001
-  }, 1000)
 })
 
 const openInNewTab = () => {
@@ -115,128 +114,37 @@ const toggle3dBuildings = () => {
   }
 }
 
-let scene: any = null
-let camera: any = null
-const pos = ref(-0.7563779)
+const useSatteliteView = ref(true)
+
+const toggleMapView = () => {
+  if (!mapRef.value) return
+  useSatteliteView.value = !useSatteliteView.value
+
+  if (useSatteliteView.value) {
+    mapRef.value?.setStyle('mapbox://styles/mapbox/satellite-streets-v12')
+  } else {
+    mapRef.value?.setStyle('mapbox://styles/mapbox/dark-v11')
+  }
+}
+
+let scene: THREE.Scene | null = null
+let camera: THREE.Camera | null = null
+
+setInterval(() => {
+  sensorsStore.gpsPosition.lat += 0.00001
+}, 500)
+
 watch(mapRef, () => {
-  mapRef.value?.setCenter([gpsPosition.value.lat, gpsPosition.value.lng])
-  mapRef.value?.on('style.load', () => {
+  if (!mapRef.value) return
+  mapRef.value.setCenter([gpsPosition.value.lat, gpsPosition.value.lng])
+  mapRef.value.on('style.load', () => {
     if (use3dBuildings.value) add3dBuildings()
   })
 
-  const modelOrigin = [-0.7563779, 48.0879123]
-  const modelAltitude = 10
-  const modelRotate = [Math.PI / 2, 0, 0]
+  const layer = getMapBox3DDroneModelLayer(camera, scene, mapRef.value)
 
-  const modelAsMercatorCoordinate = mapboxgl.MercatorCoordinate.fromLngLat(
-    modelOrigin,
-    modelAltitude
-  )
-
-  const modelTransform = {
-    translateX: modelAsMercatorCoordinate.x,
-    translateY: modelAsMercatorCoordinate.y,
-    translateZ: modelAsMercatorCoordinate.z,
-    rotateX: modelRotate[0],
-    rotateY: modelRotate[1],
-    rotateZ: modelRotate[2],
-    /* Since the 3D model is in real world meters, a scale transform needs to be
-     * applied since the CustomLayerInterface expects units in MercatorCoordinates.
-     */
-    scale: modelAsMercatorCoordinate.meterInMercatorCoordinateUnits() * 6
-  }
-
-  const customLayer = {
-    id: '3d-model',
-    type: 'custom',
-    renderingMode: '3d',
-    onAdd: function (map, gl) {
-      camera = new THREE.Camera()
-      scene = new THREE.Scene()
-
-      // create two three.js lights to illuminate the model
-      const directionalLight = new THREE.DirectionalLight(0xffffff)
-      directionalLight.position.set(0, -70, 100).normalize()
-      scene.add(directionalLight)
-
-      const directionalLight2 = new THREE.DirectionalLight(0xffffff)
-      directionalLight2.position.set(0, 70, 100).normalize()
-      scene.add(directionalLight2)
-
-      // use the three.js GLTF loader to add the 3D model to the three.js scene
-      const loader = new GLTFLoader()
-      loader.load('/mq-9_reaper/scene.gltf', (gltf) => {
-        scene.add(gltf.scene)
-      })
-      this.map = map
-
-      // use the Mapbox GL JS map canvas for three.js
-      this.renderer = new THREE.WebGLRenderer({
-        canvas: map.getCanvas(),
-        context: gl,
-        antialias: true
-      })
-
-      this.renderer.autoClear = false
-    },
-    render: function (gl, matrix) {
-      const rotationX = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(1, 0, 0),
-        modelTransform.rotateX
-      )
-      const rotationY = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(0, 1, 0),
-        modelTransform.rotateY
-      )
-      const rotationZ = new THREE.Matrix4().makeRotationAxis(
-        new THREE.Vector3(0, 0, 1),
-        modelTransform.rotateZ
-      )
-      const modelAsMercatorCoordinate2 =
-        mapboxgl.MercatorCoordinate.fromLngLat(
-          [pos.value, modelOrigin[1]],
-          modelAltitude
-        )
-      const modelTransform2 = {
-        translateX: modelAsMercatorCoordinate2.x,
-        translateY: modelAsMercatorCoordinate2.y,
-        translateZ: modelAsMercatorCoordinate2.z,
-        rotateX: modelRotate[0],
-        rotateY: modelRotate[1],
-        rotateZ: modelRotate[2],
-        /* Since the 3D model is in real world meters, a scale transform needs to be
-         * applied since the CustomLayerInterface expects units in MercatorCoordinates.
-         */
-        scale:
-          modelAsMercatorCoordinate2.meterInMercatorCoordinateUnits() * 3
-      }
-
-      const m = new THREE.Matrix4().fromArray(matrix)
-      const l = new THREE.Matrix4()
-        .makeTranslation(
-          modelTransform2.translateX,
-          modelTransform2.translateY,
-          modelTransform2.translateZ
-        )
-        .scale(
-          new THREE.Vector3(
-            modelTransform2.scale,
-            -modelTransform2.scale,
-            modelTransform2.scale
-          )
-        )
-        .multiply(rotationX)
-        .multiply(rotationY)
-        .multiply(rotationZ)
-
-      camera.projectionMatrix = m.multiply(l)
-      this.renderer.resetState()
-      this.renderer.render(scene, camera)
-      this.map.triggerRepaint()
-    }
-  }
   mapRef.value?.on('style.load', () => {
-    mapRef.value?.addLayer(customLayer, 'waterway-label')
+    mapRef.value?.addLayer(layer, 'waterway-label')
   })
 })
 
@@ -265,8 +173,7 @@ const goToInitialZoom = () => {
       v-else
       :map-id="`map-gps-${uniqueMapId}`"
       :options="{
-        // style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
-        style: 'mapbox://styles/mapbox/dark-v11',
+        style: 'mapbox://styles/mapbox/satellite-streets-v12', // style URL
         pitch: 45,
         // center: [gpsPosition.lat, gpsPosition.lng], // starting position
         zoom: 15.75, // starting zoom,
@@ -316,6 +223,13 @@ const goToInitialZoom = () => {
         title="Toggle 3D buildings"
       >
         <Box :size="24" />
+      </button>
+      <button
+        class="absolute top-[10rem] right-0 z-50 bg-neutral-900 rounded p-1.5 mt-2.5 mr-2.5 text-primary"
+        @click="toggleMapView"
+        title="Toggle map view"
+      >
+        <Map :size="24" />
       </button>
     </MapboxMap>
   </div>
