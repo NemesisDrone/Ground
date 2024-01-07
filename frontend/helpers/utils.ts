bac0bb9fdae60499ae93a9f491a3510abdfc4022
0xPierre <0xpierre@protonmail.com>
2024-01-07 18:12:13 +0100

    refacto: pause/play/time format

--- frontend/helpers/utils.ts
+++ frontend/helpers/utils.ts
@@ -5,3 +5,12 @@ import { camelize, getCurrentInstance, toHandlerKey } from 'vue'
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs))
 }
+
+export const formatTimer = (time: number) => {
+  let seconds = time / 1000
+  const minutes = Math.floor(seconds / 60)
+  const remainingSeconds = seconds % 60
+  const twoDecimalSeconds = remainingSeconds.toFixed(2).padStart(5, '0')
+
+  return `${String(minutes).padStart(2, '0')}:${twoDecimalSeconds}`
+}
--- frontend/pages/operations/replay.vue
+++ frontend/pages/operations/replay.vue
@@ -1,7 +1,15 @@
 <script lang="ts" setup>
 import ReplayMap from '~/components/Operations/Replay/ReplayMap.vue'
 import { useReplayStore } from '~/store/replayStore'
-import { Ban, Clock10, PauseCircle } from 'lucide-vue-next'
+import {
+  Play,
+  Pause,
+  Ban,
+  Clock10,
+  PauseCircle,
+  RotateCcw
+} from 'lucide-vue-next'
+import { formatTimer } from '../../helpers/utils'
 
 definePageMeta({
   layout: 'sidebar',
@@ -98,62 +106,67 @@ watch(
   }
 )
 
-const formatTime = (time: number) => {
-  let seconds = time / 1000
-  const minutes = Math.floor(seconds / 60)
-  const remainingSeconds = seconds % 60
-  const twoDecimalSeconds = remainingSeconds.toFixed(2).padStart(5, '0')
-
-  return `${String(minutes).padStart(2, '0')}:${twoDecimalSeconds}`
-}
+const isReplayEnd = computed(() => {
+  return (
+    replayStore.lastFrameIndex === replayStore.frames.length - 1 &&
+    !replayStore.isPlaying
+  )
+})
 </script>
 <template>
   <div class="w-full h-[100vh]">
-    <div class="h-5/6 w-full flex gap-4">
+    <div class="h-full w-full flex gap-4">
       <div class="w-3/5">
-        <ReplayMap />
-      </div>
-      <div class="w-2/5">Other</div>
-    </div>
-    <div class="h-1/6 w-full flex gap-4 p-4">
-      <div class="w-4/5 h-full flex flex-col">
-        <div class="mt-5">
-          <UiSlider
-            v-model="sliderTime"
-            :max="replayStore.frames[replayStore.frames.length - 1].time"
-            :min="replayStore.frames[0].time"
-            :step="100"
-          />
+        <div class="h-5/6">
+          <ReplayMap />
         </div>
-        <div class="w-full mt-4 flex">
-          <div class="rounded bg-neutral-900 flex p-2 items-center">
-            <Clock10 :size="18" class="mr-2" />
-            <div>
-              {{ formatTime(replayStore.currentTime) }} /
-              {{
-                formatTime(
-                  replayStore.frames[replayStore.frames.length - 1].time
-                )
-              }}
+        <div class="h-1/6 w-full flex flex-col gap-4 p-4">
+          <div class="mt-5">
+            <UiSlider
+              v-model="sliderTime"
+              :max="replayStore.frames[replayStore.frames.length - 1].time"
+              :min="replayStore.frames[0].time"
+              :step="100"
+            />
+          </div>
+          <div class="w-full flex gap-2">
+            <div class="rounded bg-neutral-900 flex p-2 items-center">
+              <Clock10 :size="18" class="mr-2" />
+              <div>
+                {{ formatTimer(replayStore.currentTime) }} /
+                {{
+                  formatTimer(
+                    replayStore.frames[replayStore.frames.length - 1].time
+                  )
+                }}
+              </div>
+            </div>
+            <div
+              class="rounded bg-neutral-900 items-center justify-center hover:bg-neutral-800"
+            >
+              <Play
+                v-if="!replayStore.isPlaying && !isReplayEnd"
+                :size="22"
+                class="m-2 cursor-pointer"
+                @click="play"
+              />
+              <RotateCcw
+                v-if="!replayStore.isPlaying && isReplayEnd"
+                :size="22"
+                class="m-2 cursor-pointer"
+                @click="play"
+              />
+              <Pause
+                v-if="replayStore.isPlaying"
+                :size="22"
+                class="m-2 cursor-pointer"
+                @click="pause"
+              />
             </div>
           </div>
         </div>
       </div>
-      <div class="w-1/5 h-full">
-        <div class="flex gap-2">
-          <UiButton @click="play">
-            <Play :size="18" class="mr-2" />
-            Play
-          </UiButton>
-          <UiButton variant="destructive" @click="pause">
-            <PauseCircle :size="18" class="mr-2" />
-            Pause
-          </UiButton>
-          <UiButton @click="stop">
-            <Ban :size="18" class="mr-2" />
-            Stop
-          </UiButton>
-        </div>
+      <div class="w-2/5">
         {{ replayStore.isPlaying ? 'Playing' : 'Not playing' }}
       </div>
     </div>
