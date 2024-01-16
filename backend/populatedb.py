b3b71d0641af09b6c2046f709cbee2bda90f7baa
0xPierre <0xpierre@protonmail.com>
2024-01-16 23:10:21 +0100

    feat: clear database before populating

--- backend/populatedb.py
+++ backend/populatedb.py
@@ -1,18 +1,24 @@
 import django
 
 django.setup()
-from apps.user.models import User
-from apps.drone.models import DroneSettings
+
+"""
+For every model we clear the database before populating it
+"""
 
 """
 Create a first user in the database
 """
+from apps.user.models import User
+User.objects.all().delete()
 user = User.objects.create_user(identifier="Nemesis", password="nemesis")
 print('User Nemesis with password "nemesis" created')
 
 """
 Create a drone settings in the database
 """
+from apps.drone.models import DroneSettings
+DroneSettings.objects.all().delete()
 DroneSettings().save()
 print("Drone settings created")
 
