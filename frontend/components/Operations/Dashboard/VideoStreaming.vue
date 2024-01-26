3ea304e33edc7ae347976534dbe8e9ada6fd0836
N1coc4colA <n1coc4cola@gmail.com>
2024-01-26 19:49:46 +0100

    refacto: disable YPR drawing & optis.
    Disable yaw/pitch/roll drawing.
    Use img for image instead of drawing on canvas.
    Code clean up.

--- frontend/components/Operations/Dashboard/VideoStreaming.vue
+++ frontend/components/Operations/Dashboard/VideoStreaming.vue
@@ -4,6 +4,7 @@ import { storeToRefs } from 'pinia'
 import { useSensorsStore } from '~/store/sensors'
 
 const route = useRoute()
+const sensorsStore = useSensorsStore()
 
 const openInNewTab = () => {
   window.open('/fullscreen/video', '_blank')
@@ -14,7 +15,6 @@ const closeWindow = () => {
 }
 
 const paint = (
-  showMessage: bool,
   has_image: bool,
   x: number,
   y: number,
@@ -22,65 +22,95 @@ const paint = (
   src,
   canvas
 ) => {
-  console.log(x.toString() + ' ' + y.toString() + ' ' + r.toString())
+  return
 
-  const ctx = canvas.getContext('2d')
+  const ctx = canvas.getContext("2d")
   if (!ctx) {
-    console.log('Failed to get context!')
     return
   }
+
+  const shc = "black"
+  const shw = 8
+  const fgc = "#20B256"
+  const fgw = 2
+  const bgc = "#171717"
 
   const size = Math.min(canvas.clientHeight, canvas.clientWidth)
   const dx = (canvas.clientWidth - size) / 2
   const dy = (canvas.clientHeight - size) / 2
 
+  //context.clearRect(0, 0, size, size);
+
   if (has_image) {
     let img = new Image(size, size)
-    img.src = src
+    img.src = src;
     ctx.drawImage(img, dx, dy, size, size)
   } else {
-    // ctx.fillStyle = 'grey'
-    ctx.fillStyle = '#171717'
+    ctx.fillStyle = bgc
     ctx.fillRect(dx, dy, size, size)
-  }
-
-  if (showMessage) {
-    // c'est pas bo
-    // ctx.fillStyle = "blue";
-    // ctx.font = "bold 20px serif";
-    // ctx.fillText("No video available.", dx, 20);
-    // ctx.stroke();
   }
 
   const begin = size / 5
   const axis_len = begin * 3
 
-  ctx.strokeStyle = '#20B256'
-  ctx.lineWidth = 2
-  ctx.fillStyle = '#20B256'
-  ctx.font = 'bold 12px monospace' // Beware that roll numbers drawing depends on this on a constant manner.
+  ctx.strokeStyle = fgc
+  ctx.lineWidth = fgw
+  ctx.fillStyle = fgc
+  ctx.font = "bold 12px monospace"
 
   let x_offset = begin * (x % 1)
   let y_offset = begin * (y % 1)
 
   // X axis
+  ctx.strokeStyle = shc
+  ctx.lineWidth = shw
+  ctx.beginPath()
+  ctx.moveTo(dx + begin, dy + size - 10)
+  ctx.lineTo(dx + begin + axis_len, dy + size - 10)
+  ctx.stroke()
+
+  ctx.strokeStyle = fgc
+  ctx.lineWidth = fgw
   ctx.beginPath()
   ctx.moveTo(dx + begin, dy + size - 10)
   ctx.lineTo(dx + begin + axis_len, dy + size - 10)
   ctx.stroke()
 
   // Y axis
+  ctx.strokeStyle = shc
+  ctx.lineWidth = shw
   ctx.beginPath()
   ctx.moveTo(dx + 10, dy + begin)
   ctx.lineTo(dx + 10, dy + begin + axis_len)
   ctx.stroke()
 
-  const x_pos = dx + x_offset + begin
-  let value = Math.floor(x) - 2
+  ctx.strokeStyle = fgc
+  ctx.lineWidth = fgw
+  ctx.beginPath()
+  ctx.moveTo(dx + 10, dy + begin)
+  ctx.lineTo(dx + 10, dy + begin + axis_len)
+  ctx.stroke()
+
+  const x_pos = dx + x_offset + begin;
+  let value = Math.floor(x) - 2;
   for (let i = x % 1 < -0.4 ? 1 : 0; i < 3 + (x % 1 > 0.4 ? 0 : 1); i++) {
     let text = value.toFixed(2)
 
+    ctx.shadowColor = shc
+    ctx.shadowBlur = shw
     ctx.fillText(text, x_pos + begin * i + 4, dy + size - 14)
+    ctx.shadowBlur = 0
+    ctx.fillText(text, x_pos + begin * i + 4, dy + size - 14)
+
+    ctx.strokeStyle = shc
+    ctx.lineWidth = shw
+    ctx.beginPath()
+    ctx.moveTo(x_pos + begin * i, dy + size - 10)
+    ctx.lineTo(x_pos + begin * i, dy + size - 20)
+    ctx.stroke()
+
+    ctx.strokeStyle = fgc
+    ctx.lineWidth = fgw
     ctx.beginPath()
     ctx.moveTo(x_pos + begin * i, dy + size - 10)
     ctx.lineTo(x_pos + begin * i, dy + size - 20)
@@ -88,12 +118,26 @@ const paint = (
     value++
   }
 
-  const y_pos = dy + begin + y_offset
-  value = Math.floor(y) - 2
+  const y_pos = dy + begin + y_offset;
+  value = Math.floor(y) - 2;
   for (let i = y % 1 < -0.2 ? 1 : 0; i < 3 + (y % 1 > 0.2 ? 0 : 1); i++) {
-    let text = value.toFixed(2)
+    let text = value.toFixed(2);
 
+    ctx.shadowColor = shc
+    ctx.shadowBlur = shw
     ctx.fillText(text, dx + 14, y_pos + begin * i - 4)
+    ctx.shadowBlur = 0
+    ctx.fillText(text, dx + 14, y_pos + begin * i - 4)
+
+    ctx.strokeStyle = shc
+    ctx.lineWidth = shw
+    ctx.beginPath()
+    ctx.moveTo(dx + 10, y_pos + begin * i)
+    ctx.lineTo(dx + 20, y_pos + begin * i)
+    ctx.stroke()
+
+    ctx.strokeStyle = fgc
+    ctx.lineWidth = fgw
     ctx.beginPath()
     ctx.moveTo(dx + 10, y_pos + begin * i)
     ctx.lineTo(dx + 20, y_pos + begin * i)
@@ -103,15 +147,21 @@ const paint = (
 
   // Draw roll.
   const radius = 50
-  const r_x = dx + size - radius - ctx.measureText('-000.00').width
+  const r_x = dx + size - radius - ctx.measureText("-000.00").width
   const r_y = dy + 25 + radius
 
   // Arc first.
+  ctx.strokeStyle = shc
+  ctx.lineWidth = shw
   ctx.beginPath()
   ctx.arc(r_x, r_y, radius, Math.PI * 1.01, Math.PI * -0.01, false)
   ctx.stroke()
 
-  const pixSize = window.getComputedStyle(canvas)
+  ctx.strokeStyle = fgc
+  ctx.lineWidth = fgw
+  ctx.beginPath()
+  ctx.arc(r_x, r_y, radius, Math.PI * 1.01, Math.PI * -0.01, false)
+  ctx.stroke()
 
   // Draw values
   const floored = Math.floor(r)
@@ -127,30 +177,65 @@ const paint = (
     const up = s * (radius + 23)
     const left = c * (radius + 23)
 
+    ctx.shadowColor = shc
+    ctx.shadowBlur = shw
     ctx.fillText(
       text,
-      r_x +
-        left -
+      r_x + left -
         (left >= (radius + 23) / 2
           ? 16
           : ctx.measureText(text).width - 16),
       r_y + up + (up < (radius + 23) / 7 ? 10 : -16)
     )
+    ctx.shadowBlur = 0
+    ctx.fillText(
+      text,
+      r_x + left -
+        (left >= (radius + 23) / 2
+          ? 16
+          : ctx.measureText(text).width - 16),
+      r_y + up + (up < (radius + 23) / 7 ? 10 : -16)
+    )
+
+    ctx.strokeStyle = shc
+    ctx.lineWidth = shw
     ctx.beginPath()
     if (value % 45 == 0) {
       ctx.moveTo(
-        r_x + c * ((4 * radius) / 10),
-        r_y + s * ((4 * radius) / 10)
-      )
+        r_x + (c * 4 * radius) / 10,
+        r_y + (s * 4 * radius) / 10
+      );
     } else if (value % 10 == 0) {
       ctx.moveTo(
-        r_x + c * ((6 * radius) / 10),
-        r_y + s * ((6 * radius) / 10)
+        r_x + (c * 6 * radius) / 10,
+        r_y + (s * 6 * radius) / 10
+      );
+    } else {
+      ctx.moveTo(
+        r_x + (c * 8 * radius) / 10,
+        r_y + (s * 8 * radius) / 10
+      )
+    }
+    ctx.lineTo(r_x + c * radius, r_y + s * radius)
+    ctx.stroke()
+
+    ctx.strokeStyle = fgc
+    ctx.lineWidth = fgw
+    ctx.beginPath()
+    if (value%45 == 0) {
+      ctx.moveTo(
+        r_x + (c * 4 * radius) / 10,
+        r_y + (s * 4 * radius) / 10
+      )
+    } else if (value%10 == 0) {
+      ctx.moveTo(
+        r_x + (c * 6 * radius) / 10,
+        r_y + (s * 6 * radius) / 10
       )
     } else {
       ctx.moveTo(
-        r_x + c * ((8 * radius) / 10),
-        r_y + s * ((8 * radius) / 10)
+        r_x + (c * 8 * radius) / 10,
+        r_y + (s * 8 * radius) / 10
       )
     }
     ctx.lineTo(r_x + c * radius, r_y + s * radius)
@@ -160,76 +245,41 @@ const paint = (
 }
 
 onMounted(() => {
-  const canvas = document.querySelector('#imager')
-  canvas.width = canvas.clientWidth
-  canvas.height = canvas.clientHeight
-
   let yaw = 0.0
   let pitch = 0.0
   let roll = 0.0
-  let blob = null
-  let url = ''
 
   let ws = new WebSocket(
     useRuntimeConfig().public.NVS_WEB_SOCKET_URL as string
   )
 
-  function make_painting() {
-    paint(
-      ws == null ? true : ws.readyState != 1,
-      url != '',
-      yaw,
-      pitch,
-      roll,
-      url,
-      canvas
-    )
-  }
-
   function ws_connect() {
-    ws.addEventListener('error', (event) => {
-      console.error('WS ERROR: ', event.message)
+    ws.addEventListener("error", event => {
+      console.error("WS ERROR: ", event.message)
       ws.close()
       ws = null
-      make_painting()
-    })
+    });
 
-    ws.addEventListener('close', (event) => {
+    ws.addEventListener("close", event => {
       ws.close()
       ws = null
-      make_painting()
-    })
+    });
 
-    ws.addEventListener('message', (event) => {
-      event.data.arrayBuffer().then((res) => {
-        let { _yaw, _pitch, _roll } = storeToRefs(useSensorsStore())
+    ws.addEventListener('message', event => {
+      event.data.arrayBuffer().then(res => {
+        document.querySelector("#imager").src = (window.URL || window.webkitURL).createObjectURL(new Blob([new Uint8Array(res)], { type: 'image/jpeg' }))
+      });
+    });
+  };
 
-        blob = new Blob([new Uint8Array(res)], { type: 'image/jpeg' })
-        url = (window.URL || window.webkitURL).createObjectURL(blob)
-        yaw = _yaw
-        pitch = _pitch
-        roll = _roll
-
-        make_painting()
-      })
-    })
-  }
-
-  addEventListener('resize', (event) => {
-    canvas.width = canvas.clientWidth
-    canvas.height = canvas.clientHeight
-
-    make_painting()
-  })
-
-  make_painting()
   ws_connect()
 })
 </script>
 
 <template>
   <div class="relative h-full m-0 p-0 overflow-hidden">
-    <canvas class="w-full object-cover h-full m-auto" id="imager"></canvas>
+    <img class="w-full object-cover h-full m-auto" id="imager"/>
+    <canvas class="w-full object-cover h-full m-auto" id="drawing"></canvas>
     <button
       class="absolute top-0 right-0 z-50 bg-neutral-900 rounded p-1.5 mt-2.5 mr-2.5 text-primary"
       v-if="route.path !== '/fullscreen/video'"
